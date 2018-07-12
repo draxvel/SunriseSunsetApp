48ed9a7dda8c0584f794af0a9d5c4d8ad8eef02b
draxvel <ntv3331998@gmail.com>
2018-07-12 22:30:20 +0300

    Refactored time format.

--- app/src/main/java/com/tkachuk/sunrisesunsetapp/util/UTCIntoLocalTime.kt
+++ app/src/main/java/com/tkachuk/sunrisesunsetapp/util/UTCIntoLocalTime.kt
@@ -6,11 +6,12 @@ import java.util.*
 class UTCIntoLocalTime {
     companion object {
         fun intoLocalTime(time: String): String{
-            val df = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+            val lastIndex = if(time.substring(time.length-2).equals("AM")) " AM" else " PM"
+            val df = SimpleDateFormat("hh:mm", Locale.ENGLISH)
             df.timeZone = TimeZone.getTimeZone("UTC")
             val date = df.parse(time)
             df.timeZone = TimeZone.getDefault()
-            return df.format(date)
+            return df.format(date)+lastIndex
         }
     }
 }
